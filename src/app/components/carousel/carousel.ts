b1bb41ee22322af122d6495c12425304e97e16db
KHANGUI Chedli <khangui.mohamedchedli@esprit.tn>
2025-11-21 18:00:41 +0100

    update

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,5 +1,5 @@
 // carousel.component.ts
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Slide } from '../../models/models';
 
@@ -10,11 +10,62 @@ import { Slide } from '../../models/models';
   templateUrl: './carousel.html',
   styleUrl: './carousel.scss'
 })
-export class Carousel {
+export class Carousel implements OnInit, OnDestroy {
   @Input() slides: Slide[] = [];
   current = 0;
+  isAutoPlay = true;
+  private autoPlayTimer: any;
 
   ngOnInit() {
-    setInterval(() => this.current = (this.current + 1) % this.slides.length, 6000);
+    this.startAutoPlay();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoPlay();
+  }
+
+  startAutoPlay() {
+    if (this.slides.length > 0) {
+      this.autoPlayTimer = setInterval(() => {
+        if (this.isAutoPlay && this.slides.length > 0) {
+          this.current = (this.current + 1) % this.slides.length;
+        }
+      }, 5000);
+    }
+  }
+
+  stopAutoPlay() {
+    if (this.autoPlayTimer) {
+      clearInterval(this.autoPlayTimer);
+    }
+  }
+
+  next() {
+    if (this.slides.length > 0) {
+      this.current = (this.current + 1) % this.slides.length;
+      this.isAutoPlay = false;
+    }
+  }
+
+  prev() {
+    if (this.slides.length > 0) {
+      this.current = (this.current - 1 + this.slides.length) % this.slides.length;
+      this.isAutoPlay = false;
+    }
+  }
+
+  goToSlide(index: number) {
+    if (index >= 0 && index < this.slides.length) {
+      this.current = index;
+      this.isAutoPlay = false;
+    }
+  }
+
+  onMouseEnter() {
+    this.isAutoPlay = false;
+  }
+
+  onMouseLeave() {
+    this.isAutoPlay = true;
   }
 }
